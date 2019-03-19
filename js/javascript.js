b73f18a6ab2c0630bb29445ae89a7ce8daa7d1bc
Rafal <rafusv2@gmail.com>
2019-03-19 15:15:43 +0100

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,4 @@
-addEventListener("load", function() {
+addEventListener("load", function() {   	           //document.addEventListener("DOMContentLoaded", function(event) {
 	var audio = document.getElementById("audio");
 	audio.volume = 0.1;
 });
